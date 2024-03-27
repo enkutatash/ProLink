79ac5c969b13634ceb5ecea52162d4cb2c04f8d6
Enkutatash <enkutatasheshetu96.com>
2024-03-27 22:13:40 +0300

    x

---
+++ skillswap/lib/firebase/skills.dart
@@ -0,0 +1,11 @@
+List<String> skills = [
+  "UI/UX",
+  "Django",
+  "Flutter",
+  "React",
+  "Machine Learning",
+  "Artificial Intelligence",
+  "Competative Programming",
+  "Project Management",
+  
+];
