f354b6903190b251b59ea552f0ab142095ff95fa
Enkutatash <enkutatasheshetu96.com>
2024-03-27 15:13:52 +0300

    home page setup

---
+++ skillswap/lib/homepage/homepage.dart
@@ -0,0 +1,81 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:skillswap/homepage/homescreen.dart';
+import 'package:skillswap/homepage/message.dart';
+import 'package:skillswap/homepage/personalproject.dart';
+import 'package:skillswap/homepage/profile.dart';
+import 'package:skillswap/homepage/search.dart';
+
+
+class Homepage extends StatefulWidget {
+  Map<String, dynamic> userdata;
+  final String userid;
+  Homepage(this.userid,this.userdata, {Key? key}) : super(key: key);
+
+  @override
+  State<Homepage> createState() => _General_ScreenState();
+}
+
+class _General_ScreenState extends State<Homepage> {
+  late List<Widget> AllScreens;
+
+  int _selectedScreen = 0;
+
+  
+  @override
+  void initState() {
+    super.initState();
+   
+    AllScreens = [
+      HomeScreen(),
+      SearchScreen(),
+      Message(),
+      ProjectScreen(),
+      ProfilePage(widget.userdata, widget.userid),
+      
+    ];
+  }
+
+  void _currentScreen(int index) {
+    setState(() {
+      _selectedScreen = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+          return Scaffold(
+            body: AllScreens[_selectedScreen],
+            bottomNavigationBar: BottomNavigationBar(
+              onTap: _currentScreen,
+              currentIndex: _selectedScreen,
+              selectedItemColor: const Color(0xFF6055D8),
+              unselectedItemColor: Colors.grey,
+              iconSize: 30,
+              items: const <BottomNavigationBarItem>[
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.home),
+                  label: "",
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.search),
+                  label: "",
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.message_rounded),
+                  label: "",
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.newspaper_rounded),
+                  label: "",
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.person),
+                  label: "",
+                ),
+              ],
+            ),
+          );
+  }
+}
---
+++ skillswap/lib/homepage/profile.dart
@@ -0,0 +1,147 @@
+import 'package:flutter/material.dart';
+import 'package:skillswap/Front/signin.dart';
+import 'package:skillswap/homepage/homepage.dart';
+
+class ProfilePage extends StatelessWidget {
+  Map<String, dynamic> userdata;
+  String userid;
+
+  ProfilePage(this.userdata, this.userid, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+    return Scaffold(
+      body: Padding(
+        padding:
+            const EdgeInsets.only(left: 20.0, right: 20, top: 70, bottom: 30),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Stack(children: [
+              CircleAvatar(
+                  radius: 60.0,
+                  backgroundImage: NetworkImage(userdata['profilePic'])),
+              
+            ]),
+            const SizedBox(height: 20.0),
+            Text(
+              "${userdata['First']} ${userdata['Last']}",
+              style: const TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 5.0),
+            Text(
+              userdata['Email'],
+              style: const TextStyle(
+                fontSize: 16.0,
+                color: Colors.grey,
+              ),
+            ),
+            const SizedBox(height: 20.0),
+            NavigationButton(
+              move: Homepage(userid, userdata),
+              text: 'Profile',
+              icon: Icons.person,
+              showArrowIcon: true,
+            ),
+            NavigationButton(
+              move: Homepage(userid, userdata),
+              text: 'Settings',
+              icon: Icons.settings,
+              showArrowIcon: true,
+            ),
+            NavigationButton(
+              move: Homepage(userid, userdata),
+              text: 'Contact',
+              icon: Icons.mail,
+              showArrowIcon: true,
+            ),
+            NavigationButton(
+              move: Homepage(userid, userdata),
+              text: 'Share App',
+              icon: Icons.share,
+              showArrowIcon: true,
+            ),
+            NavigationButton(
+              move: Homepage(userid, userdata),
+              text: 'Help',
+              icon: Icons.help,
+              showArrowIcon: true,
+            ),
+            const Spacer(),
+            SizedBox(
+              width: width * 0.4,
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => SignInPage()),
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color(0XFF6055D8)),
+                child: const Text(
+                  "Sign Out",
+                  style: TextStyle(color: Colors.white),
+                ), // Add your button text here
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class BottomIconButton extends StatelessWidget {
+  final IconData icon;
+  final bool isActive;
+  final VoidCallback onPressed;
+
+  const BottomIconButton({
+    Key? key,
+    required this.icon,
+    required this.isActive,
+    required this.onPressed,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      onPressed: onPressed,
+      icon: Icon(
+        icon,
+        color: isActive ? Colors.blue : Colors.grey,
+      ),
+    );
+  }
+}
+
+class NavigationButton extends StatelessWidget {
+  final String text;
+  final IconData icon;
+  final bool showArrowIcon;
+  Widget move;
+  NavigationButton({
+    Key? key,
+    required this.move,
+    required this.text,
+    required this.icon,
+    required this.showArrowIcon,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      onTap: () {
+        Navigator.push(context, MaterialPageRoute(builder: (context) => move));
+      },
+      leading: Icon(icon),
+      title: Text(text),
+      trailing: showArrowIcon ? Icon(Icons.arrow_forward_ios) : null,
+    );
+  }
+}
