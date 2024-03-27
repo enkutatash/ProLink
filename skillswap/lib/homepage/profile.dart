177c9ce837bc059a4903645b66fa3cb9fd3ac08b
Enkutatash <enkutatasheshetu96.com>
2024-03-28 01:01:55 +0300

    skill

--- skillswap/lib/homepage/profile.dart
+++ skillswap/lib/homepage/profile.dart
@@ -6,81 +6,105 @@ class ProfilePage extends StatelessWidget {
   Map<String, dynamic> userdata;
   String userid;
 
-  ProfilePage(this.userdata, this.userid, {super.key});
+  ProfilePage(this.userdata, this.userid, {Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final height = MediaQuery.of(context).size.height;
     final width = MediaQuery.of(context).size.width;
     return Scaffold(
       body: Padding(
-        padding:
-            const EdgeInsets.only(left: 20.0, right: 20, top: 70, bottom: 30),
+        padding: const EdgeInsets.fromLTRB(20.0, 50.0, 20.0, 30.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             Align(
               alignment: Alignment.topRight,
-              child: IconButton(onPressed: (){
-                Navigator.push(
+              child: IconButton(
+                onPressed: () {
+                  Navigator.push(
                     context,
                     MaterialPageRoute(builder: (context) => SignInPage()),
                   );
-              },icon: Icon(Icons.logout_rounded,color: Colors.red,size: 30,),),
+                },
+                icon: Icon(
+                  Icons.logout_rounded,
+                  color: Colors.red,
+                  size: 30,
+                ),
+              ),
             ),
-            Stack(children: [
-              CircleAvatar(
+            Stack(
+              children: [
+                CircleAvatar(
                   radius: 60.0,
-                  backgroundImage: NetworkImage(userdata['profilePic'])),
-              
-            ]),
-            const SizedBox(height: 20.0),
+                  backgroundImage: NetworkImage(userdata['profilePic']),
+                ),
+              ],
+            ),
+            SizedBox(height: 20.0),
             Text(
               "${userdata['First']} ${userdata['Last']}",
-              style: const TextStyle(
+              style: TextStyle(
                 fontSize: 20.0,
                 fontWeight: FontWeight.bold,
               ),
             ),
-            const SizedBox(height: 5.0),
+            SizedBox(height: 5.0),
             Text(
               userdata['Email'],
-              style: const TextStyle(
+              style: TextStyle(
                 fontSize: 16.0,
                 color: Colors.grey,
               ),
             ),
-            const SizedBox(height: 20.0),
-            NavigationButton(
-              move: Homepage(userid, userdata),
-              text: 'Profile',
-              icon: Icons.person,
-              showArrowIcon: true,
+            SizedBox(height: height * 0.03),
+            const Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                "Skills",
+                style: TextStyle(
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
             ),
-            NavigationButton(
-              move: Homepage(userid, userdata),
-              text: 'Settings',
-              icon: Icons.settings,
-              showArrowIcon: true,
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                // Set the background color of the container
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.75),
+                    offset: Offset(0, 5),
+                    blurRadius: 5.0,
+                    spreadRadius: 2.0,
+                  ),
+                ],
+              ),
+              child: Expanded(
+                child: Column(
+                  children: [
+                    SizedBox(height: height * 0.002),
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: Colors.black,
+                      ),
+                      height: height * 0.3,
+                      child: ListView.builder(
+                        itemCount: userdata['Skills'].length,
+                        itemBuilder: (context, index) {
+                          Map<String, dynamic> skill =
+                              userdata['Skills'][index];
+                          return SkillLevel(skill['skill']!, skill['level']!);
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ),
-            NavigationButton(
-              move: Homepage(userid, userdata),
-              text: 'Contact',
-              icon: Icons.mail,
-              showArrowIcon: true,
-            ),
-            NavigationButton(
-              move: Homepage(userid, userdata),
-              text: 'Share App',
-              icon: Icons.share,
-              showArrowIcon: true,
-            ),
-            NavigationButton(
-              move: Homepage(userid, userdata),
-              text: 'Help',
-              icon: Icons.help,
-              showArrowIcon: true,
-            ),
-          
           ],
         ),
       ),
@@ -88,52 +112,15 @@ class ProfilePage extends StatelessWidget {
   }
 }
 
-class BottomIconButton extends StatelessWidget {
-  final IconData icon;
-  final bool isActive;
-  final VoidCallback onPressed;
-
-  const BottomIconButton({
-    Key? key,
-    required this.icon,
-    required this.isActive,
-    required this.onPressed,
-  }) : super(key: key);
+class SkillLevel extends StatelessWidget {
+  String skill, level;
+  SkillLevel(this.skill, this.level, {super.key});
 
   @override
   Widget build(BuildContext context) {
-    return IconButton(
-      onPressed: onPressed,
-      icon: Icon(
-        icon,
-        color: isActive ? Colors.blue : Colors.grey,
-      ),
-    );
-  }
-}
-
-class NavigationButton extends StatelessWidget {
-  final String text;
-  final IconData icon;
-  final bool showArrowIcon;
-  Widget move;
-  NavigationButton({
-    Key? key,
-    required this.move,
-    required this.text,
-    required this.icon,
-    required this.showArrowIcon,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return ListTile(
-      onTap: () {
-        Navigator.push(context, MaterialPageRoute(builder: (context) => move));
-      },
-      leading: Icon(icon),
-      title: Text(text),
-      trailing: showArrowIcon ? Icon(Icons.arrow_forward_ios) : null,
+    return Row(
+      children: [Text(skill),
+       GestureDetector(onTap: (){},child: Text(level))],
     );
   }
 }
